07780e3b1c6b952b796e2f54b778d794a387981e
Will Miller <willmiller629@gmail.com>
2021-01-16 15:40:14 -0500

    still working throuhg cloud deployment

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(_dirname + '/dist/angular-tour-of-heroes'));
-app.get('/*', function(req,res){
-  res.sendFile(path.join(_dirname+
-    '/dist/angular-tour-of-heroes/index.html'));
-});
+app.use(express.static('./dist/angular-tour-of-heroes'));
+app.get('/*', (req,res) =>
+  res.sendFile('index/html', {root: 'dist/angular-tour-of-heroes/'}),
+);
 app.listen(process.env.PORT || 8080);
